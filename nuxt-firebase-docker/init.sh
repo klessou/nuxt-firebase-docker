1eab6dde708ead0d4673f331ca67c11cd4003d7f
David Piry <david.piry@videdressing.com>
2018-05-11 17:22:37 +0200

    Update documentation

--- nuxt-firebase-docker/init.sh
+++ nuxt-firebase-docker/init.sh
@@ -7,5 +7,5 @@ if [ -f "$FILE" ]; then
      npm install
      npm run dev
 else
-    echo "$DIR is Empty: please run "docker-compose run nuxt-firebase-docker vue init nuxt-community/starter-template ." before"
+    echo "$DIR is Empty: please run this command before : 'docker-compose run web vue init nuxt-community/starter-template .'"
 fi
