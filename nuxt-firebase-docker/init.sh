ab5fd5a1d763196ddf7c26fb4fceafe46fe6e463
David Piry <david.piry@videdressing.com>
2018-05-11 17:40:33 +0200

    Weird, but we now need this package... probably an error with nuxt dependencies (I need to stop using the starter template probably)

--- nuxt-firebase-docker/init.sh
+++ nuxt-firebase-docker/init.sh
@@ -3,9 +3,11 @@ set timeout 360
 FILE="/code/package.json"
 
 if [ -f "$FILE" ]; then
-     echo "Starting project"
-     npm install
-     npm run dev
+    echo "Starting project"
+    npm install opencollective
+    npm install
+    echo "Run project"  
+    npm run dev
 else
     echo "$DIR is Empty: please run this command before : 'docker-compose run web vue init nuxt-community/starter-template .'"
 fi
