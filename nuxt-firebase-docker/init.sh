dff53d2cd33a90cd6510a1dfe2ea8d5f28b3f7c8
David Piry <davidpiry@gmail.com>
2022-02-13 19:50:47 +0100

    Support last nuxt version

--- nuxt-firebase-docker/init.sh
+++ nuxt-firebase-docker/init.sh
@@ -6,7 +6,7 @@ if [ -f "$FILE" ]; then
     echo "Starting project"
     npm install opencollective
     npm install
-    echo "Run project"  
+    echo "Run project"
     npm run dev
 else
     echo "$DIR is Empty: please run "docker-compose run nuxt-firebase-docker npx create-nuxt-app ." before"
